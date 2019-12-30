44dece93d9e1c5011af95ee622ca70162d8be77f
vishnu <vishrayne@gmail.com>
2019-12-30 10:55:00 +0530

    Update react-native version to 0.59.9 for android examples
    Signed-off-by: vishnu <vishrayne@gmail.com>

--- examples/ListView/index.android.js
+++ examples/ListView/index.android.js
@@ -1,43 +1,15 @@
 /*
  * This Android demo shows how to integrate `PullToRefreshViewAndroid` with `ScrollableParallaxView`.
  */
-import React, { Component } from 'react'
-import {
-  AppRegistry,
-  PullToRefreshViewAndroid,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, View } from "react-native";
 
-import Talks from './Talks';
+import Talks from "./Talks";
 
 class AndroidExample extends Component {
   render() {
-    return (
-      <PullToRefreshViewAndroid
-        ref={ref => { this._pullToRefresh = ref }}
-        style={{ flex: 1 }}
-        colors={['#000', '#999', '#fff']}
-        progressBackgroundColor={'#fff'}
-        enabled={true}
-        refreshing={false}
-        onRefresh={() => {
-          this._pullToRefresh.getInnerViewNode().setNativeProps({ refreshing: true });
-          setTimeout(() => {
-            this._pullToRefresh.getInnerViewNode().setNativeProps({ refreshing: false });
-          }, 5000);
-        }}>
-        <Talks
-          key="talks"
-          onScroll={(e) => {
-            if (e.nativeEvent.contentOffset.y <= 0) {
-              this._pullToRefresh.getInnerViewNode().setNativeProps({ enabled: true });
-            } else {
-              this._pullToRefresh.getInnerViewNode().setNativeProps({ enabled: false });
-            }
-          }}/>
-      </PullToRefreshViewAndroid>
-    );
+    return <Talks key="talks" />;
   }
 }
 
-AppRegistry.registerComponent('ListViewExample', () => AndroidExample);
+AppRegistry.registerComponent("ListViewExample", () => AndroidExample);
